c6bdecd4d0c22c4fa07f0d442272d3c0d7cc9514
Kim Altintop <kim.altintop@xing.com>
2009-10-21 20:48:11 +0800

    test for exception behavior

--- test/basic_integration_test.rb
+++ test/basic_integration_test.rb
@@ -10,8 +10,11 @@ class BasicIntegrationTest < Test::Unit::TestCase
     @pid = Process.fork do
       worker = Gearman::Worker.new("localhost:4730")
       worker.add_ability("pingpong") {|data, job| "pong" }
+      worker.add_ability("crash") {|data, job| raise Exception.new("BOOM!") }
       worker.work
     end
+
+    @client = Gearman::Client.new("localhost:4730")
   end
 
   def teardown
@@ -19,12 +22,24 @@ class BasicIntegrationTest < Test::Unit::TestCase
   end
 
   def test_ping_job
-    client = Gearman::Client.new("localhost:4730")
     task = Gearman::Task.new("pingpong", "ping")
 
     task.on_complete do |response|
       assert_equal "pong", response
     end
-    client.run task
+    @client.run task
+  end
+
+  def test_exception_in_worker
+    task = Gearman::Task.new("crash", "doesntmatter")
+
+    warning_given = nil
+    failed = false
+    task.on_warning {|warning| warning_given = warning }
+    task.on_fail { failed = true}
+    @client.run task
+
+    assert_not_nil warning_given
+    assert true, failed
   end
 end
