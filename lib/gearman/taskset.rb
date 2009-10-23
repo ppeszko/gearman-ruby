a35dcdd6346d76fd912c1161ec5707faaddd7eba
Kim Altintop <kim.altintop@xing.com>
2009-10-23 23:51:20 +0800

    taskset is basically an array

--- lib/gearman/taskset.rb
+++ lib/gearman/taskset.rb
@@ -1,36 +1,11 @@
 module Gearman
-  class Taskset
+  class Taskset < ::Array
 
     def self.create(task_or_taskset)
-      return task_or_taskset if task_or_taskset.kind_of?(Taskset)
-      new([task_or_taskset])
+      [*task_or_taskset]
     end
 
-    def initialize(tasks = [])
-      @tasks = tasks
-    end
-
-    def add(task)
-      @tasks << task
-      self
-    end
-    alias :<< :add
+    alias :add :<<
     alias :add_task :add
-
-    def each
-      @tasks.each {|task| yield task }
-    end
-
-    def pop
-      @tasks.pop
-    end
-
-    def shift
-      @tasks.shift
-    end
-
-    def size
-      @tasks.size
-    end
   end
 end
